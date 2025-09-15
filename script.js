31ba01fb57f98d60ddd16ceab8c7c0c718b92f08
Tiago França <github@tiagofranca.com>
2025-09-15 20:33:39 -0300

    fir refactor
    	modified:   index.html
    	modified:   script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-function changeVisibility() {
+function changeVisibility(btn) {
     const p = document.querySelector('.not-visible, .visible');
-    if (!p) return;
-    p.classList.toggle('not-visible');
-    p.classList.toggle('visible');
-    let btn = document.getElementsByClassName('secret')[0];
-    if (p.classList.toggle('visible')){
-
-        btn.innerHTML =  'Esconder'
-    } else {
-        btn.innerHTML =  'Mostrar'
-
+    if (!p) {
+        return;
     }
+
+    let toShow = p.classList.contains('not-visible');
+
+    if (toShow) {
+        p.classList.remove('not-visible');
+        p.classList.add('visible');
+        btn.innerHTML =  'Esconder';
+        return
+    }
+
+    p.classList.add('not-visible');
+    p.classList.remove('visible');
+    btn.innerHTML =  'Mostrar';
 }
 
 
