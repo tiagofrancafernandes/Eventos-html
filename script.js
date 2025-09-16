2a16a80b1fd036cafa6fcc768410837994357a78
Tiago França <github@tiagofranca.com>
2025-09-15 21:58:24 -0300

    new refactor
    	modified:   index.html
    	modified:   script.js

--- script.js
+++ script.js
@@ -21,3 +21,39 @@ function changeVisibility(btn) {
 function setBgColor(color) {
     document.body.style.backgroundColor = color;
 }
+
+function setTextColor(color) {
+    document.body.style.color = color;
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('[until-load]').forEach((el) => {
+        el.removeAttribute('until-load');
+    });
+
+    document.querySelectorAll('[ocultar]').forEach((el) => {
+        let toHide = ['true', '1', 'on'].includes(String(el.getAttribute('ocultar') || '').toLowerCase());
+
+        el.style.display = toHide ? 'none' : '';
+    });
+
+    document.querySelectorAll('[data-tipo-de-acao][data-acao-em="click"]').forEach((el) =>
+        el.addEventListener('click', (event) => {
+            let novaCor = event.target?.dataset?.novaCor || '';
+            let tipoAcao = event.target?.dataset?.tipoDeAcao || '';
+
+            if (!tipoAcao) {
+                return;
+            }
+
+            if (tipoAcao === 'altera cor de FUNDO body') {
+                setBgColor(novaCor);
+                return;
+            }
+
+            if (tipoAcao === 'altera cor do TEXTO do body') {
+                setTextColor(novaCor);
+            }
+        })
+    );
+});
